8751719e2a792468731d0a6fec4577b6ea5b43cb
Jim Fowler <fowler@math.osu.edu>
2019-01-04 10:12:27 -0500

    helper script to build both versions

---
+++ build-both.sh
@@ -0,0 +1,16 @@
+#! env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+last=${@:$#} # last parameter 
+other=${*%${!#}} # all parameters except the last
+exercises=`echo $last|sed 's/.tex$/e.tex/'`
+solutions=`echo $last|sed 's/.tex$/s.tex/'`
+
+ruby $DIR/build-worksheet.rb $other --output=$exercises
+pdflatex $exercises
+pdflatex $exercises
+
+ruby $DIR/build-worksheet.rb $other --solutions --output=$solutions
+pdflatex $solutions
+pdflatex $solutions
